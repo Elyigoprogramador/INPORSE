66df2a0707e988e61ccf02deb325a232a89b24c9
Elyigoprogramador <hernandezrodri83@gmail.com>
2024-10-01 21:35:24 -0600

    laptop yigo

--- INPORSE/Module1.vb
+++ INPORSE/Module1.vb
@@ -3,7 +3,7 @@ Module Module1
     Public mysqlconexion As New MySqlConnection
     Public Sub funcionConectarBD()
         If mysqlconexion.State = ConnectionState.Closed Then
-            mysqlconexion.ConnectionString = "Server=127.0.0.1; User ID=root; Password=admin123; Port=3306; Database=INPORSE;"
+            mysqlconexion.ConnectionString = "Server=localhost; User ID=root; Password=admin123; Port=3308; Database=INPORSE;"
             mysqlconexion.Open()
         End If
     End Sub
