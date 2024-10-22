4ec4a8896cb0270c10900b0f8d370c2f9a61c472
Elyigoprogramador <hernandezrodri83@gmail.com>
2024-10-21 19:06:11 -0600

    102

--- INPORSE/My Project/Application.Designer.vb
+++ INPORSE/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.INPORSE.CON
+            Me.MainForm = Global.INPORSE.Formfactura
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
